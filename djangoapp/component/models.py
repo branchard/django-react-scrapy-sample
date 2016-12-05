ce177310eed043aa49eaf293eaaf3ca4881dc05b
Benoit <benoit.branchard@gmail.com>
2016-12-05 16:43:55 +0100

    componets models improvements

--- djangoapp/component/models.py
+++ djangoapp/component/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Component(models.Model):
     name = models.CharField(max_length=50)
     brand = models.ForeignKey(
@@ -11,31 +12,46 @@ class Component(models.Model):
     class Meta:
         abstract = True
 
+
 class Brand(models.Model):
     name = models.CharField(max_length=50)
 
+
 class Processor(Component):
-    frequency = models.FloatField()#Ghz
+    frequency = models.FloatField()  # Ghz
     cores = models.IntegerField()
     socket = models.ForeignKey(
         'Socket',
         on_delete=models.CASCADE,
     )
 
+
 class Motherboard(Component):
     ramSlots = models.IntegerField()
-    maxRam = models.IntegerField()# Go
+    maxRam = models.IntegerField()  # Go
+    ramtype = models.ForeignKey(
+        'RamType',
+        on_delete=models.CASCADE,
+    )
+    ramfrequency = models.ManyToManyField("RamFrequency")  # une carte mere est compatible avec plusieurs frequences de ram
     socket = models.ForeignKey(
         'Socket',
         on_delete=models.CASCADE,
     )
+    pcitypes = models.ManyToManyField("PciType")  # une carte mere peut avoir plusieurs slots PCI
+    formfactor = models.ForeignKey(
+        'MotherBoardFormFactor',
+        on_delete=models.CASCADE,
+    )
+
 
 class Socket(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=10)
+
 
 class Ram(Component):
-    capacity = models.IntegerField()# Go par barrette, capacity * quantity = total memory
-    quantity = models.IntegerField()# nombre de barrette
+    capacity = models.IntegerField()  # Go par barrette, capacity * quantity = total memory
+    quantity = models.IntegerField()  # nombre de barrette
     ramtype = models.ForeignKey(
         'RamType',
         on_delete=models.CASCADE,
@@ -45,8 +61,63 @@ class Ram(Component):
         on_delete=models.CASCADE,
     )
 
+
 class RamFrequency(models.Model):
-    frequency = models.IntegerField()# Mhz
+    frequency = models.IntegerField()  # Mhz
+
 
 class RamType(models.Model):
-    typeName = models.CharField(max_length=10)# DDR2, DDR3, DDR4
+    typeName = models.CharField(max_length=10)  # DDR2, DDR3, DDR4
+
+
+class GraphicCard(Component):
+    memory = models.IntegerField()  # Go
+    pcitype = models.ForeignKey(
+        'PciType',
+        on_delete=models.CASCADE,
+    )
+
+
+class PciType(models.Model):
+    name = models.CharField(max_length=50)  # PCI-E 3.0, PCI-E 2.0
+
+
+class Case(Component):
+    weight = models.FloatField()  # in Kg
+    width = models.IntegerField()  # in mm
+    height = models.IntegerField()  # in mm
+    depth = models.IntegerField()  # in mm
+    motherBoardFormFactors = models.ManyToManyField("MotherBoardFormFactor")  # un boitier peut etre compatible avec plusieurs Carte mere
+    powerSupplyFormFactor = models.ForeignKey(
+        'PowerSupplyFormFactor',
+        on_delete=models.CASCADE,
+    )
+
+
+class MotherBoardFormFactor(models.Model):
+    name = models.CharField(max_length=10)
+
+
+class PowerSupply(Component):
+    watts = models.IntegerField()  # in watt
+    modular = models.BooleanField()
+    factorForm = models.ForeignKey(
+        'PowerSupplyFormFactor',
+        on_delete=models.CASCADE,
+    )
+
+
+class PowerSupplyFormFactor(models.Model):
+    name = models.CharField(max_length=10)
+
+
+class HardDrive(Component):
+    capacity = models.IntegerField()  # Go
+    hardDriveType = models.ForeignKey(
+        'HardDriveType',
+        on_delete=models.CASCADE,
+    )
+
+
+class HardDriveType(models.Model):
+    name = models.CharField(max_length=10)  # SSD ou HDD
