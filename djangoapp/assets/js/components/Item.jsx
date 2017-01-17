8797faca6fd1af76c8767aa94531bcda50370cfa
branchard <benoit.branchard@gmail.com>
2017-01-17 21:02:42 +0100

    [front] ItemList IU Improvement

--- djangoapp/assets/js/components/Item.jsx
+++ djangoapp/assets/js/components/Item.jsx
@@ -5,8 +5,35 @@ import Spinner from "./Spinner";
 class ItemList extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            currentSelectedItem: null, // must contain product
+            currentHoveredItem: null // must contain product
+        };
+
+        this.onItemClick = this.onItemClick.bind(this);
+        this.onItemHover = this.onItemHover.bind(this);
+        this.onItemHoverLeave = this.onItemHoverLeave.bind(this);
         this.openingToogleHandler = this.openingToogleHandler.bind(this);
         this.renderProductsTable = this.renderProductsTable.bind(this);
+    }
+
+    onItemClick(product){
+        this.setState({
+            currentSelectedItem: product
+        });
+    }
+
+    onItemHover(product){
+        this.setState({
+            currentHoveredItem: product
+        });
+    }
+
+    onItemHoverLeave(){
+        this.setState({
+            currentHoveredItem: null
+        });
     }
 
     openingToogleHandler(event) {
@@ -34,21 +61,19 @@ class ItemList extends React.Component {
                 let regexExec = /^(.*)\.(.*)$/.exec(collumn.apiName);
                 if(regexExec)
                 {
-                    console.log("if");
-                    console.log(regexExec[1]);
                     _row = row[regexExec[1]][regexExec[2]];
                 }else{
                     _row = row[collumn.apiName];
                 }
 
                 productCollumns.push(
-                    <td key={key} >
+                    <td key={key} onClick={() => that.onItemClick(row)} onMouseEnter={() => that.onItemHover(row)} onMouseLeave={that.onItemHoverLeave} >
                         {_row}
                     </td>
                 );
             });
             productsRows.push(
-                <tr key={key} >
+                <tr key={key} className={row == that.state.currentSelectedItem ? "actived" : ""} >
                     {productCollumns}
                 </tr>
             );
@@ -88,6 +113,37 @@ class ItemList extends React.Component {
                 </div>
             )
         }
+
+        let currentSelectedItemDiv;
+        if(this.state.currentSelectedItem || this.state.currentHoveredItem){
+            let currentProduct = this.state.currentHoveredItem || this.state.currentSelectedItem;
+            currentSelectedItemDiv = (
+                <div className="right-countainer">
+                    <div className="item-description">
+                        <span className="item-label">{currentProduct.name}</span>
+                        <span className="item-descr">Processeur Socket 1151 - Quad Core - Cache 6 Mo - Skylake - Ventirad non inclus</span>
+                        <span className="item-price">
+                            <span className="label label-danger">259.90 €</span>
+                        </span>
+                    </div>
+                    <div className="item-preview">
+                        <img src="http://lorempicsum.com/simpsons/300/300/5"/>
+                    </div>
+                </div>
+            );
+        }else{
+            /* No Description */
+            currentSelectedItemDiv = (
+                <div className="right-countainer">
+                    <div className="item-description">
+
+                    </div>
+                    <div className="item-preview">
+                    </div>
+                </div>
+            );
+        }
+
         return (
             <article className={"item" + (open ? " item-opened" : "")}>
                 <div className="frame item-top row" onClick={this.openingToogleHandler}>
@@ -95,18 +151,7 @@ class ItemList extends React.Component {
                         <h3 className="item-type-name">
                             <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>{this.props.name}</h3>
                     </div>
-                    <div className="right-countainer">
-                        <div className="item-description">
-                            <span className="item-label">Intel Core i5-6600K (3.5 GHz)</span>
-                            <span className="item-descr">Processeur Socket 1151 - Quad Core - Cache 6 Mo - Skylake - Ventirad non inclus</span>
-                            <span className="item-price">
-                                <span className="label label-danger">259.90 €</span>
-                            </span>
-                        </div>
-                        <div className="item-preview">
-                            <img src="http://lorempicsum.com/simpsons/300/300/5"/>
-                        </div>
-                    </div>
+                    {currentSelectedItemDiv}
                 </div>
                 {selectMenu}
             </article>
