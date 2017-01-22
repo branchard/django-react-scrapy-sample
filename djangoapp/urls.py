fad14490f3f0c66168c4f1e10c0354ad32a8a882
branchard <benoit.branchard@gmail.com>
2017-01-22 01:23:29 +0100

    [back] Urls minor fix

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -11,7 +11,6 @@ router = routers.DefaultRouter()
 # router.register(r'groups', views.GroupViewSet)
 
 router.register(r'components', components_views.ComponentsViewSet)
-router.register(r'shops', shops_views.ShopsViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
